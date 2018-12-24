0ffe3a7591cc2b9eb5d97f6496fe0760ad2597df
Alexandr Selishchev <44421225+selishchev@users.noreply.github.com>
2018-12-24 22:25:51 +0300

    Update case8.py

--- case8.py
+++ case8.py
@@ -1,20 +1,75 @@
-import numpy as np
-import matplotlib.pyplot as plt
-import pylab
-from matplotlib import mlab
-
-
-def _9():
-    currency = []
-    date = []
-    day = 1
-    with open('usd9.txt') as f_in:
-        for item in f_in:
-            currency.append(float(item))
-            date.append(day)
-            day += 1
-    plt.plot(date, currency, marker='*')
+ import numpy as np
+ import matplotlib.pyplot as plt
+ import pylab
+ import math
+ import pylab
+ from matplotlib import mlab
+ 
+ 
+ def _1():
+     cy = []
+     dat = []
+     day = 1
+     with open('usd9.txt') as f:
+         for item in f:
+             cy.append(float(item))
+             dat.append(day)
+             day += 1
+     plt.plot(day, cy)
+     plt.show()
+ 
+ 
+ def _2():
+     currency = []
+     currency_e = []
+     date = []
+     day = 1
+     with open('euro1.txt') as f_in1:
+         for char in f_in1:
+             currency_e.append(float(char))
+     with open('usd9.txt') as f_in:
+         for item in f_in:
+             currency.append(float(item))
+             date.append(day)
+             day += 1
+     plt.plot(date, currency, currency_e)
     plt.show()
+
+
+def _8(): 
+currency = [] 
+date = [] 
+day = 1 
+with open('usd9.txt') as f_in: 
+for item in f_in: 
+currency.append(float(item)) 
+date.append(day) 
+day += 1 
+plt.title('USD-Rubles in 2018') 
+plt.xlabel('Days') 
+plt.ylabel('Rubles for USD') 
+plt.plot(date, currency) 
+plt.axis([0, 250, 55, 72]) 
+plt.grid(True) 
+plt.show() 
+
+
+def _9(): 
+currency = [] 
+currency_e = [] 
+date = [] 
+day = 1 
+with open('euro1.txt') as f_in1: 
+for char in f_in1: 
+currency_e.append(float(char)) 
+with open('usd9.txt') as f_in: 
+for item in f_in: 
+currency.append(float(item)) 
+date.append(day) 
+day += 1 
+plt.plot(date, currency, marker='*') 
+plt.plot(date, currency_e, marker='o') 
+plt.show()
 
 
 def _10():
